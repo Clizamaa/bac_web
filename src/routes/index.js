756d3a26914a117fa3e79f7556eed4921dee0b16
christian Lizama <cfla86@gmail.com>
2022-08-18 10:09:44 -0400

    formulario de contacto

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,47 @@
 const { Router} = require ('express')
+const nodemailer = require ('nodemailer')
 const router = Router();
 
-router.post('/send-email', (req, res) =>{
-    console.log(req.body)
+router.post('/send-email', async (req, res) =>{
+    // console.log(req.body)
+    //destructuración de objetos
+    const{name, email, phone, message} = req.body; 
+
+    contentHTML = `
+    
+        <h1>Información de Usuario</h1>
+        <ul>
+            <li>Nombre: ${name}</li>
+            <li>Email: ${email}</li>
+            <li>Teléfono: ${phone}</li>
+        </ul>
+           <p> Mensaje: ${message}</p>
+
+        `;
+
+       const transporter = nodemailer.createTransport({
+        host: "mail.bacchile.cl",
+        port: 587,
+        secure: false,
+        auth:{
+            user: 'contacto@bacchile.cl',
+            pass: 'bacchile2022'
+        },
+        tls:{
+            rejectUnauthorized: false
+        }
+       });
+
+      const info= await transporter.sendMail({
+            from: '"Contacto WEB Bac Chile" <contacto@bacchile.cl> ',
+            to: 'cfla86@gmail.com',
+            subject: 'Nuevo mensaje',
+            html: contentHTML
+      });
+
+      console.log('Message sent', info.messageId);
+       
+
     res.send('recibido')
 
 
